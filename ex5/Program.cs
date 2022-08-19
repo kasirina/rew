298939e493c6df8500b068eea04015e0cb0471e9
«IRIS» <kasir@gmail.com>
2022-08-19 17:11:42 +0300

    add ex5 MACHA

---
+++ ex5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("УРААА! Это же Маша)))");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
