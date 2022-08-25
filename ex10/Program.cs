72a4d7bb03ad1eeb9bdfc941e17dd270b2de7ef7
«IRIS» <kasir@gmail.com>
2022-08-26 00:40:59 +0300

    сохранить  массив2

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {651, 22, 314, 4009, 5, 61, 7579, 83, 19};
+
+int n = array.Length; //   возвращает длину массива
+int find = 61;
+
+int index = 0;
+
+while (index<n)
+{
+   if(array[index]==find)
+   {
+        Console.WriteLine(index);
+        break; //   оператор прерывания - для нахождения первого элемента
+   }
+   //index= index+1;
+   index++;
+}
